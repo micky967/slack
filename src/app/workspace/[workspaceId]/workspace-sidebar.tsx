eb2aac4a9b04d5fede9f3c0c10d4ac4da2a9b46f
Manjit Lider <micky967@yahoo.com>
2024-10-04 01:34:01 -0700

    man Troy

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -58,9 +58,9 @@ export const WorkspaceSidebar = () => {
         />
 
         <div className="flex flex-col px-2 mt-3 space-y-1">
-          <p className="text-white font-bold">Manjit</p>
+          <p className="text-white text-sm font-bold ml-3">Manjit</p>
           {/* <SidebarItem label="Threads" icon={MessageSquareText} id="threads" /> */}
-          <p className="text-white font-bold">Troy</p>
+          <p className="text-white text-sm font-bold ml-3">Troy</p>
           {/* <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" /> */}
         </div>
 
